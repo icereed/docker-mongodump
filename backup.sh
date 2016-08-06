a0118fc316f89267314dbfda7ece1fd0b9a79e70
Icereed <domi@icereed.net>
2016-08-06 20:08:52 +0200

    Redirect via stdout

--- backup.sh
+++ backup.sh
@@ -7,6 +7,6 @@ echo "Job started: $(date)"
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$DATE.tar.gz"
 
-mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --gzip --archive $FILE
+mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT --gzip --archive > $FILE
 
 echo "Job finished: $(date)"
